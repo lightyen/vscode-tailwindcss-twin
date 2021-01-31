16626e402d5b8b4da36b1aa17d324680ce055f05
lightyen <lightyen0123@gmail.com>
2021-01-31 21:16:44 +0800

    variants

--- server/variants.ts
+++ server/variants.ts
@@ -1,11 +1,11 @@
-// This is unused module
+// unused module
 
-function findRightBracket(classes: string, start = 0) {
+function findRightBracket(classes: string, start = 0, end = classes.length, brackets = ["(", ")"]) {
 	const stack = []
-	for (let index = start; index < classes.length; index++) {
-		if (classes[index] === "(") {
+	for (let index = start; index < end; index++) {
+		if (classes[index] === brackets[0]) {
 			stack.push(index)
-		} else if (classes[index] === ")") {
+		} else if (classes[index] === brackets[1]) {
 			if (stack.length === 0) {
 				return undefined
 			}
@@ -27,11 +27,11 @@ function spreadVariantGroups(classes: string, context = "", importantContext = f
 	const results: string[] = []
 	classes = classes.slice(start, end).trim()
 
-	const reg = /([\w-]+:)|([\w-./]+!?)|\(|(\S+)/g
+	const reg = /([\w-]+:)|(\w+\[)|([\w-./]+!?)|\(|(\S+)/g
 	let match: RegExpExecArray
 	const baseContext = context
 	while ((match = reg.exec(classes))) {
-		const [, variant, className, weird] = match
+		const [, variant, cssProperty, className, weird] = match
 		if (variant) {
 			context += variant
 
@@ -44,21 +44,31 @@ function spreadVariantGroups(classes: string, context = "", importantContext = f
 				const closeBracket = findRightBracket(classes, reg.lastIndex)
 				if (typeof closeBracket !== "number") {
 					throw `"${classes}" except to find a ')' to match the '('`
-				} else {
-					const importantGroup = classes[closeBracket + 1] === "!"
-					results.push(
-						...spreadVariantGroups(
-							classes,
-							context,
-							importantContext || importantGroup,
-							reg.lastIndex + 1,
-							closeBracket,
-						),
-					)
-					reg.lastIndex = closeBracket + (importantGroup ? 2 : 1)
-					context = baseContext
 				}
+
+				const importantGroup = classes[closeBracket + 1] === "!"
+				results.push(
+					...spreadVariantGroups(
+						classes,
+						context,
+						importantContext || importantGroup,
+						reg.lastIndex + 1,
+						closeBracket,
+					),
+				)
+				reg.lastIndex = closeBracket + (importantGroup ? 2 : 1)
+				context = baseContext
 			}
+		} else if (cssProperty) {
+			const closeBracket = findRightBracket(classes, match.index, classes.length, ["[", "]"])
+			if (typeof closeBracket !== "number") {
+				throw `"${classes}" except to find a ']' to match the '['`
+			}
+			const importantGroup = classes[closeBracket + 1] === "!"
+			const css = classes.slice(match.index, closeBracket + 1)
+			results.push(context + css + (importantGroup || importantContext ? "!" : ""))
+			reg.lastIndex = closeBracket + (importantGroup ? 2 : 1)
+			context = baseContext
 		} else if (className) {
 			const tail = !className.endsWith("!") && importantContext ? "!" : ""
 			results.push(context + className + tail)
@@ -69,25 +79,23 @@ function spreadVariantGroups(classes: string, context = "", importantContext = f
 			const closeBracket = findRightBracket(classes, match.index)
 			if (typeof closeBracket !== "number") {
 				throw `"${classes}" except to find a ')' to match the '('`
-			} else {
-				const importantGroup = classes[closeBracket + 1] === "!"
-				results.push(
-					...spreadVariantGroups(
-						classes,
-						context,
-						importantContext || importantGroup,
-						match.index + 1,
-						closeBracket,
-					),
-				)
-				reg.lastIndex = closeBracket + (importantGroup ? 2 : 1)
 			}
+
+			const importantGroup = classes[closeBracket + 1] === "!"
+			results.push(
+				...spreadVariantGroups(
+					classes,
+					context,
+					importantContext || importantGroup,
+					match.index + 1,
+					closeBracket,
+				),
+			)
+			reg.lastIndex = closeBracket + (importantGroup ? 2 : 1)
 		}
 	}
 
 	return results
 }
 
-const handleVariantGroups = (classes: string) => spreadVariantGroups(classes).join(" ")
-
-export { handleVariantGroups, spreadVariantGroups }
+export { spreadVariantGroups }
