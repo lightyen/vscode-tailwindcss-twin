58542b05c94defe34a09dac35ca88cf7bd68c5a2
lightyen <lightyen0123@gmail.com>
2021-10-26 15:21:35 +0800

    remove eval()

--- src/common/index.ts
+++ src/common/index.ts
@@ -7,3 +7,13 @@ export function md5(value: string) {
 export function escapeRegexp(expression: string) {
 	return expression.replace(/[/\\^$*+?.()|[\]{}]/g, "\\$&")
 }
+
+/** accept strings: `1/4` */
+export function calcFraction(value: string): number {
+	const i = value.indexOf("/")
+	if (i === -1) return NaN
+	const a = Number(value.slice(0, i))
+	const b = Number(value.slice(i + 1))
+	if (Number.isNaN(a) || Number.isNaN(b)) return NaN
+	return a / b
+}
