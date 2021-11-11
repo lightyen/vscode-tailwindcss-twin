d691401734e99ac0ea1a46e59398f75d65c69508
lightyen <lightyen0123@gmail.com>
2021-11-11 14:49:38 +0800

    remove ts-node

---
+++ config.js
@@ -0,0 +1,6 @@
+const { register } = require("@swc-node/register/register")
+const { readDefaultTsConfig } = require("@swc-node/register/read-default-tsconfig")
+const path = require("path")
+
+register(readDefaultTsConfig(path.resolve("webpack.tsconfig.json")))
+module.exports = require("./webpack.config.ts").default
