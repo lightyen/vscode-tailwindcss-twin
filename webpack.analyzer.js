47e8360a4b7bf288d3910918fd4f700c698ff04c
lightyen <lightyen0123@gmail.com>
2021-11-19 13:49:01 +0800

    Bump

--- webpack.analyzer.js
+++ webpack.analyzer.js
@@ -1,6 +1,5 @@
 const { register } = require("@swc-node/register/register")
 const { readDefaultTsConfig } = require("@swc-node/register/read-default-tsconfig")
 const path = require("path")
-
-register(readDefaultTsConfig(path.resolve("webpack.tsconfig.json")))
+register(readDefaultTsConfig(path.resolve("tsconfig.json")))
 module.exports = require("./webpack.analyzer.ts").default
