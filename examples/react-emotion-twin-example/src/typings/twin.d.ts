64a8644d255327219a7d6589cdff0d1b2cb0be3b
lightyen <lightyen0123@gmail.com>
2021-11-25 14:32:49 +0800

    update example

--- examples/react-emotion-twin-example/src/typings/twin.d.ts
+++ examples/react-emotion-twin-example/src/typings/twin.d.ts
@@ -1,51 +1,101 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="@emotion/react/types/css-prop" />
+
 declare module "twin.macro" {
-	import React from "react"
-
-	export type TwStyle = Record<string, string | number | TwStyle>
-
-	export interface TemplateFn<R> {
-		(template: Readonly<TemplateStringsArray>): R
+	// TODO: Custom theme
+	declare module "@emotion/react" {
+		export interface Theme {
+			colors: {
+				primary: string
+			}
+		}
 	}
-
-	export type StyledTags = {
-		[Tag in keyof JSX.IntrinsicElements]: TemplateFn<React.ComponentType<JSX.IntrinsicElements[Tag]>>
-	}
-
-	export interface AdditionalProps {}
-
-	interface TwComponentWrapper {
-		<ComponentProps extends {}>(component: React.ComponentType<ComponentProps>): TemplateFn<
-			React.ComponentType<ComponentProps>
-		>
-
-		<Tag extends keyof JSX.IntrinsicElements>(tag: Tag): TemplateFn<
-			React.ComponentType<AdditionalProps & JSX.IntrinsicElements[Tag]>
-		>
-	}
-
-	export interface Tw extends StyledTags, TwComponentWrapper {
-		(template: Readonly<TemplateStringsArray>): TwStyle
-	}
-
-	declare const tw: Tw
-	export default tw
 
 	declare module "react" {
 		interface Attributes {
-			/** twin.macro */
+			/** **twin.macro** */
 			tw?: string
-			/** twin.macro */
+
+			/**
+			 * **twin.macro**
+			 * @deprecated useless
+			 */
 			cs?: string
 		}
 	}
 
-	export function GlobalStyles(): React.ReactElement
+	import { CSSObject, Theme } from "@emotion/react"
+	import { CSSInterpolation } from "@emotion/serialize"
+	import { CreateStyled, StyledComponent } from "@emotion/styled"
+	import { ReactElement } from "react"
+	export const styled: CreateStyled
+	export { css, keyframes } from "@emotion/react"
 
-	export declare function theme(template: string | Readonly<TemplateStringsArray>): any
+	interface CreateStyledComponent<
+		ComponentProps extends {},
+		SpecificComponentProps extends {} = {},
+		JSXProps extends {} = {},
+	> {
+		<AdditionalProps extends {} = {}>(arr: TemplateStringsArray): StyledComponent<
+			ComponentProps & AdditionalProps,
+			SpecificComponentProps,
+			JSXProps
+		>
+	}
 
-	export declare function screen<T = string>(
-		screenValue: string | TemplateStringsArray,
-	): (styles?: string | TemplateStringsArray | TwStyle | TwStyle[]) => T
+	type StyledTags = {
+		[Tag in keyof JSX.IntrinsicElements]: CreateStyledComponent<
+			{
+				theme?: Theme
+				as?: React.ElementType
+			},
+			JSX.IntrinsicElements[Tag]
+		>
+	}
 
-	export declare const globalStyles: Record<string, unknown>
+	interface Tw {
+		(arr: TemplateStringsArray): CSSObject
+	}
+
+	interface TwComponentWrapper {
+		<ComponentProps extends {}>(component: React.ComponentType<ComponentProps>): CreateStyledComponent<
+			ComponentProps & {
+				theme?: Theme
+				as?: React.ElementType
+			}
+		>
+		<Tag extends keyof JSX.IntrinsicElements>(tag: Tag): CreateStyledComponent<
+			{
+				theme?: Theme
+				as?: React.ElementType
+			},
+			JSX.IntrinsicElements[Tag]
+		>
+	}
+
+	const tw: Tw & StyledTags & TwComponentWrapper
+	export default tw
+
+	/**
+	 * **NOTE: `Screen as a key` is a bad style.**
+	 *
+	 * Use `screen` like this:
+	 *
+	 * ```tsx
+	 * <div css={screen`sm`({ display: 'block', ...tw`inline` })} />
+	 * ```
+	 */
+	export function screen(arr: TemplateStringsArray): (arg: CSSInterpolation) => CSSObject
+
+	/** @deprecated NOT recommended. */
+	export function screen(str: string): (arg: CSSInterpolation) => CSSObject
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	export function theme(arr: TemplateStringsArray): any
+
+	/** @deprecated NOT recommended. */
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	export function theme(str: string): any
+
+	export function GlobalStyles(): ReactElement
 }
