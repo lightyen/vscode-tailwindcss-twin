e15ff1477c069d2fcf5678c5174bd2783ff721f2
lightyen <lightyen0123@gmail.com>
2022-03-25 14:24:20 +0800

    update example

--- examples/webpack-emotion-typescript/tailwind.config.ts
+++ examples/webpack-emotion-typescript/tailwind.config.ts
@@ -1,12 +1,7 @@
-import colors from "tailwindcss/colors"
-
-delete colors.lightBlue
-
 export default {
 	theme: {
 		extend: {
 			colors: {
-				...colors,
 				electric: "#db00ff",
 				ribbon: "#0047ff",
 			},
